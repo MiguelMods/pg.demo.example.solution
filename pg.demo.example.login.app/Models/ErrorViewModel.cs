68d47732e1518afd0e28620592c463ccf3faa02a
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2024-03-31 09:42:29 -0400

    Add project files.

---
+++ pg.demo.example.login.app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pg.demo.example.login.app.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
